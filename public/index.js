64b8b3399604d29de0e32c83a0a78d1c24d421c8
Thamisitech <50879028+Thamisitech@users.noreply.github.com>
2019-12-17 11:51:10 -0500

    idk

--- public/index.js
+++ public/index.js
@@ -2,6 +2,12 @@ const path = require("path");
 const express = require("express");
 const app = express();
 
-app.listen(3000, (req, res) => {
-  console.log("listening on port 300");
+app.get("", (req, res) => {
+  res.send("Thami wasn't discussing meth. Also he smells");
+});
+app.get("/thamisucks", (req, res) => {
+  res.send("Thami sucks. And Yes I have to do this while you eat");
+});
+app.listen(3000, () => {
+  console.log("Listening on port 3000");
 });
